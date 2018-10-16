0449e9900a2266f16a650eeb3cc3e758a3be39f1
BT_1N3_06 <dkurti4965@bths.edu>
2018-10-16 14:46:52 -0400

    Circle

--- src/Circle.java
+++ src/Circle.java
@@ -1,3 +1,17 @@
-public class Circle {
+public class Circle extends Shape {
+    private int radius;
+    public Circle (int radius)
+    {
+        super(type: "Cirlce") ;
+        this.radius = radius;
+    }
+    public double getArea()
+    {
+        return (Math.PI * (radius * radius));
+    }
+    public double getPerimeter()
+    {
+        return (2 * Math.PI * radius);
+    }
 
 }
