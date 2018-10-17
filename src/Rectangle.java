a705b459817b92b582b370e57e2e6e7234f583dc
BT_1N3_04 <wchen1325@bths.edu>
2018-10-17 14:46:58 -0400

    Hello

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,15 @@
-public class Rectangle {
+public class Rectangle extends Shape {
+    private int height;
+    private int width;
+    public Rectangle(int height, int width){
+        super("Rectangle");
+        this.height = height;
+        this.width = width;
+    }
+    public double getArea(){
+        return (width * height);
+    }
+    public double getPerimeter(){
+        return (2 * width + 2 * height);
+    }
 }
