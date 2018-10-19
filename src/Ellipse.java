072c0f17b63682686721c0a753175341def921db
deakurti <mamibabi7!>
2018-10-19 14:48:11 -0400

    changed ellipse

---
+++ src/Ellipse.java
@@ -0,0 +1,23 @@
+public class Ellipse extends Shape {
+    private int majoraxis;
+    private int minoraxis;
+    public Ellipse (int majoraxis)
+    {
+        super ("Ellipse");
+        this.majoraxis = majoraxis;
+    }
+    public Ellipse (int minoraxis);
+    {
+        super ("Ellipse");
+        this.minoraxis = minoraxis;
+    }
+    public double getArea()
+    {
+        return (Math.PI * (majoraxis * minoraxis));
+    }
+    public double getPerimeter()
+    {
+        return ((2 * Math.PI) * Math.sqrt(((majoraxis * majoraxis)* (minoraxis * minoraxis))/2));
+    }
+}
+
